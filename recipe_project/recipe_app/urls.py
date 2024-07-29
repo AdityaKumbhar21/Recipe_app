3ddc601e5e440d543e8de9442ef8341d49af58b5
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-07-29 12:03:40 +0530

    feat: Add "Add Recipe" functionality and displaying all the recipes n the homepage

--- recipe_project/recipe_app/urls.py
+++ recipe_project/recipe_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('add_recipe/',views.add_recipe, name='add_recipe'),
 ]
--- recipe_project/recipe_app/views.py
+++ recipe_project/recipe_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Recipe
 
 # Create your views here.
 def home(request):
-    return render(request,'recipe_app/home.html')
+    recipes = Recipe.objects.all()
+    return render(request,'recipe_app/home.html',{'recipes':recipes})
+
+def add_recipe(request):
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get('recipe_name')
+        recipe = data.get('recipe')
+        recipe_img = request.FILES.get('recipe_image')
+    
+
+        Recipe.objects.create(
+            recipe_name = recipe_name,
+            recipe = recipe,
+            recipe_image = recipe_img
+        )
+
+        return redirect('home')
+
+    return render(request,'recipe_app/add_recipe.html')\
+    
+
