6f5854220a57dde875c95858d71dfee6670e45c7
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-07-29 11:03:32 +0530

    added model and template

--- recipe_project/recipe_app/models.py
+++ recipe_project/recipe_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    recipe = models.TextField(max_length=500)
+    recipe_image = models.ImageField(upload_to='recipe_images/',null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.recipe_name}"
